78676301f4bb500a674304c11206c9bcaf4d47fd
Paul Anderson <paul@andersonloco.com>
2011-11-08 11:59:49 -0500

    Beginning of fgets() function.

--- scribble/fgettish.c
+++ scribble/fgettish.c
@@ -7,3 +7,19 @@
 //
 
 #include <stdio.h>
+#include "fgettish.h"
+
+int paul_fgets(int num, char *buf, FILE fd)
+{
+    if (fd==NULL) {
+        return -1; /* Invalid FD */
+    } else if(num<=0) {
+        return -1; /* num hsa to be something sane. */
+    }
+    
+    buf=malloc(sizeof char*num );
+    if (buf==NULL) {
+        /*Yarp! Couldn't allocate memory! */
+        return -1;
+    }
+}
