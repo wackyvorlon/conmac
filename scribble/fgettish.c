db3ce6e1b4232a1956987dcbe2105e713ebc1ccd
Paul Anderson <paul@andersonloco.com>
2011-11-08 10:09:07 -0500

    adding header stuff

---
+++ scribble/fgettish.c
@@ -0,0 +1,9 @@
+//
+//  fgettish.c
+//  scribble
+//
+//  Created by Paul Anderson on 11-11-08.
+//  Copyright (c) 2011 Anderson Locomotive Works. All rights reserved.
+//
+
+#include <stdio.h>
---
+++ scribble/scribble.h
@@ -0,0 +1,14 @@
+//
+//  scribble.h
+//  scribble
+//
+//  Created by Paul Anderson on 11-11-08.
+//  Copyright (c) 2011 Anderson Locomotive Works. All rights reserved.
+//
+
+#ifndef scribble_scribble_h
+#define scribble_scribble_h
+
+
+
+#endif
